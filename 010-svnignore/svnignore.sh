7fae7e9d99ccf5b43c4115b812327fc06fe16d31
Jan Trienes <jan.trienes@googlemail.com>
2017-01-07 21:50:53 +0100

    remove configuration redundancy in svnignore

--- 010-svnignore/svnignore.sh
+++ 010-svnignore/svnignore.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
 ##====================================================
 ## A simple script to handle svn ignore settings.
-## The script can add java and latex ignores to
-## the svn proplist.
+##
+## The script reads svnignore properties from files
+## located in svnignore.d. Each file in this directory
+## represents a proplist. This script is to be called
+## with the name of this proplist.
 ##
 ## Parametres:
 ## -a (Append) appends the new ignores to the existing ones
-## -l (LaTeX) LaTeX files to be ignored
-## -j (Java) Java files to be ignored
-## -c (C++) c++ files to be ignored
-## -cs (C#) c# files to be ignored
-## -xc (Xcode) ignores private project settings
-##
+## [proplist ...]
 ##====================================================
-# Directory which contains the arguments that shall be ignored
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Specify resources
-JAVARES=$DIR/SVNJAVAPROPSET
-TEXRES=$DIR/SVNTEXPROPSET
-CPPRES=$DIR/SVNCPPPROPSET
-CSPRES=$DIR/SVNCSHARPPROPSET
-XCRES=$DIR/SVNXCPROPSET
-#specify flags in order to handle command line arguments
-aflag=off
+config=$dir/svnignore.d
+
+append=false
 ignore_set=/dev/null
 while [ $# -gt 0 ]
 do
   case "$1" in
-    -a) aflag=on;;
-    -l) ignore_set="${ignore_set} $TEXRES";;
-    -j) ignore_set="${ignore_set} $JAVARES";;
-    -c) ignore_set="${ignore_set} $CPPRES";;
-    -cs) ignore_set="${ignore_set} $CSPRES";;
-    -xc) ignore_set="${ignore_set} $XCRES";;
-    -*) echo >&2 "usage: $0 svnignore [-a] [-l] [-j] [-c] [-cs] [-xc]"
-    break;;
+    -a)
+    append=true;;
+    -*)
+    echo >&2 "usage: $0 svnignore [-a] [proplist ...]"
+    exit 1;;
+    *)
+    if [ -e $config/$1 ]
+    then
+      ignore_set="${ignore_set} $config/$1"
+    else
+      echo >&2 "no such ignore propset: $1"
+      exit 1
+    fi
+    ;;
   esac
+
   shift
 done
 
-cat "$ignore_set" >> tmp.ignore
+cat $ignore_set >> tmp.ignore
 
-if [ $aflag = on ];
+if [ "$append" = true ];
 then
   svn proplist -v | grep -vE 'ignore|Properties' > current.ignore
   perl -lape 's/\s+//sg' current.ignore > current.ignore.formatted
